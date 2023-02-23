d31d2ce3e7f9fc2f1d97954543f2e8edcacfc550
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-02-23 17:25:39 -0300

    se realiza el ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,6 @@
+let numero = 1
+if(numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0){
+    console.log("Es divisible por uno de estos numeros");
+}else{
+    console.log("No es divisible por ninguno de estos numeros")
+}
