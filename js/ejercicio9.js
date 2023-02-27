462fbbaecf15c10a12a82d107adec1e9adce8b78
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-02-27 19:49:00 -0300

    se realiza el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,2 @@
+let frase = "Aprendiendo a programar";
+document.write(" 'Aprendiendo a programar' tiene las vocales "+frase.charAt(12)+" "+frase.charAt(3)+" "+frase.charAt(6)+" "+frase.charAt(10));
